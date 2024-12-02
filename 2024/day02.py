8dccf06636ae026f9b6c189061cb77b25d7d4a19
wise-bit <satrajit2016@gmail.com>
2024-12-02 00:15:25 -0500

    day 2

--- 2024/day02.py
+++ 2024/day02.py
@@ -1,8 +1,39 @@
 sep = "\n"  # separator
 c = 0  # counter
 
-with open("input2.txt", "r") as file:
+
+def is_safe_1(ls):
+  ds = [ls[i + 1] - ls[i] for i in range(len(ls) - 1)]
+
+  if not all(-3 <= d <= 3 and d != 0 for d in ds):
+    return False
+  if all(d > 0 for d in ds) or all(d < 0 for d in ds):
+    return True
+
+  return False
+
+
+def is_safe_2(ls):
+  if is_safe_1(ls):
+    return True
+
+  for i in range(len(ls)):
+    modded = ls[:i] + ls[i + 1:]
+    if is_safe_1(modded):
+      return True
+
+  return False
+
+
+safes = 0
+
+with open("input02.txt", "r") as file:
   lines = file.read().split(sep)
-  
+
   for line in lines:
-    pass
+    nums = list(map(int, line.split()))
+
+    if is_safe_2(nums):
+      safes += 1
+
+print(safes)
