7d82ab8fa2bebfaea51051dbadee55f7926aabbe
wise-bit <satrajit2016@gmail.com>
2024-12-02 02:01:01 -0500

    prep

---
+++ 2024/day03.py
@@ -0,0 +1,13 @@
+sep = "\n"  # separator
+c = 0  # counter
+
+left = []
+right = []
+
+with open("input03.txt", "r") as file:
+  lines = file.read().split(sep)
+  
+  for line in lines:
+    pass
+
+print(c)
