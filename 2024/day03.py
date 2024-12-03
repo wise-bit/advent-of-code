de0a1488fa9bcfbe4245f92c8555654e48322144
wise-bit <satrajit2016@gmail.com>
2024-12-03 00:30:14 -0500

    day 3

--- 2024/day03.py
+++ 2024/day03.py
@@ -1,13 +1,60 @@
 sep = "\n"  # separator
-c = 0  # counter
+total = 0  # total
 
 left = []
 right = []
 
 with open("input03.txt", "r") as file:
-  lines = file.read().split(sep)
-  
-  for line in lines:
-    pass
+  line = file.read()
 
-print(c)
+  # print(line)
+
+  i = 0
+  do = True
+
+  while i < len(line):
+    if line[i:i + 4] == "do()":
+      do = True
+      i += 4
+      continue
+
+    if line[i:i + 7] == "don't()":
+      do = False
+      i += 7
+      continue
+
+    if line[i:i + 4] == "mul(":
+      if do:
+        i += 4
+        s = i
+
+        while i < len(line) and line[i].isdigit():
+          i += 1
+        if i == s or line[i] != ',' or i >= len(line):
+          continue
+
+        num1 = int(line[s:i])
+        i += 1
+
+        s = i
+        while i < len(line) and line[i].isdigit():
+          i += 1
+        if i == s or line[i] != ')' or i >= len(line):
+          continue
+
+        num2 = int(line[s:i])
+        total += num1 * num2
+
+        i += 1
+
+      else:
+        i += 4
+        while i < len(line) and line[i] != ')':
+          i += 1
+
+        i += 1
+
+    else:
+      i += 1
+
+print(total)
