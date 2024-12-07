9734ded534145a0590ab729e615dcaa16317858a
wise-bit <satrajit2016@gmail.com>
2024-12-07 00:20:44 -0500

    day 7

--- 2024/day07.py
+++ 2024/day07.py
@@ -1,11 +1,42 @@
 sep = "\n"  # separator
 
-sol = 0
+sol_1 = 0
+sol_2 = 0
+
+
+def concatenate(a, b):
+  return int(str(a) + str(b))
+
+
+def possible(final, nums, i, total, do_concat=False):
+  if i == len(nums):
+    return total == final
+
+  if possible(final, nums, i + 1, total + nums[i], do_concat):
+    return True
+
+  if possible(final, nums, i + 1, total * nums[i], do_concat):
+    return True
+
+  if do_concat and possible(final, nums, i + 1, concatenate(total, nums[i]), do_concat):
+    return True
+
+  return False
+
 
 with open("input07.txt", "r") as file:
   lines = file.read().split(sep)
 
   for line in lines:
-    pass
+    parts = line.split(": ")
+    final = int(parts[0])
+    nums = list(map(int, parts[1].split()))
 
-print(sol)
+    if possible(final, nums, 0, 0):
+      sol_1 += final
+
+    if possible(final, nums, 0, 0, True):
+      sol_2 += final
+
+print(sol_1)
+print(sol_2)
---
+++ 2024/day08.py
@@ -0,0 +1,11 @@
+sep = "\n"  # separator
+
+sol = 0
+
+with open("input08.txt", "r") as file:
+  lines = file.read().split(sep)
+
+  for line in lines:
+    pass
+
+print(sol)
