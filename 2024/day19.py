c9c04e8064bde1d572e74ada6920553383b8eb12
wise-bit <satrajit2016@gmail.com>
2024-12-19 00:27:05 -0500

    day 19

--- 2024/day19.py
+++ 2024/day19.py
@@ -1,31 +1,69 @@
 sep = "\n"
 
 
-def fn():
-  pass
+def can_create(design, patterns):
+  n = len(design) + 1
+  cache = [False] * n
+  cache[0] = True
+
+  for sub_len in range(1, n):
+    for pattern in patterns:
+      p_len = len(pattern)
+
+      if (
+        sub_len >= p_len and cache[sub_len - p_len] and
+        design[(sub_len - p_len):sub_len] == pattern
+      ):
+        cache[sub_len] = True
+        break
+
+  return cache[len(design)]
 
 
-def part1():
-  pass
+def count_options(design, patterns):
+  n = len(design) + 1
+  cache = [0] * n
+  cache[0] = 1
+
+  for i in range(1, n):
+    for pattern in patterns:
+      p_len = len(pattern)
+
+      if i >= p_len and design[(i - p_len):i] == pattern:
+        cache[i] += cache[i - p_len]
+
+  return cache[len(design)]
 
 
-def part2():
-  pass
+def part1(patterns, designs):
+  return len([design for design in designs if can_create(design, patterns)])
+
+
+def part2(patterns, designs):
+  res = []
+  for design in designs:
+    count = count_options(design, patterns)
+    res.append(count)
+
+  return sum(res)
 
 
 # main
 
-g = []
+patterns = []
+designs = []
 
 with open("input19.txt", "r") as file:
   lines = file.read().split(sep)
+  patterns = set(lines[0].split(", "))
 
-  for line in lines:
-    pass
+  for line in lines[2:]:
+    designs.append(line)
 
+# print(patterns, designs)
 
-sol1 = 0
+sol1 = part1(patterns, designs)
 print("part 1:", sol1)
 
-sol2 = 0
+sol2 = part2(patterns, designs)
 print("part 2:", sol2)
